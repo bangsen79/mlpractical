7a26415bf86372143bdcf4c9b7d76fa12b0048d3
Fadhil Kusuma <s2650633@sms.ed.ac.uk>
2024-11-16 00:09:24 +0000

    BNRC don

--- pytorch_mlp_framework/experiment_builder.py
+++ pytorch_mlp_framework/experiment_builder.py
@@ -14,7 +14,7 @@ matplotlib.rcParams.update({'font.size': 8})
 
 class ExperimentBuilder(nn.Module):
     def __init__(self, network_model, experiment_name, num_epochs, train_data, val_data,
-                 test_data, weight_decay_coefficient, use_gpu, continue_from_epoch=-1):
+                 test_data, weight_decay_coefficient, use_gpu,learning_rate, continue_from_epoch=-1):
         """
         Initializes an ExperimentBuilder object. Such an object takes care of running training and evaluation of a deep net
         on a given dataset. It also takes care of saving per epoch models and automatically inferring the best val model
@@ -67,7 +67,7 @@ class ExperimentBuilder(nn.Module):
         print('Total number of conv layers', num_conv_layers)
         print('Total number of linear layers', num_linear_layers)
 
-        self.optimizer = optim.Adam(self.parameters(), amsgrad=False,
+        self.optimizer = optim.Adam(self.parameters(), amsgrad=False,lr=learning_rate,
                                     weight_decay=weight_decay_coefficient)
         self.learning_rate_scheduler = optim.lr_scheduler.CosineAnnealingLR(self.optimizer,
                                                                             T_max=num_epochs,
@@ -153,7 +153,8 @@ class ExperimentBuilder(nn.Module):
         #TODO write your code here
         for name, param in named_parameters:
             if 'bias' not in name:
-                layers.append(name)
+                shortened_name = '.'.join(name.split('.')[-2:])
+                layers.append(shortened_name)
                 if param.requires_grad and param.grad is not None:    
                     all_grads.append(param.grad.abs().mean().item())
             
--- pytorch_mlp_framework/train_evaluate_image_classification_system.py
+++ pytorch_mlp_framework/train_evaluate_image_classification_system.py
@@ -47,6 +47,9 @@ if args.block_type == 'conv_block':
 elif args.block_type == 'BN_block':
     processing_block_type = ConvolutionalProcessingBlockWithBN
     dim_reduction_block_type = ConvolutionalDimensionalityReductionBlockWithBN
+elif args.block_type == 'BNRC_block':
+    processing_block_type = ConvolutionalProcessingBlockWithBNRC
+    dim_reduction_block_type = ConvolutionalProcessingBlockWithBNRC
 elif args.block_type == 'empty_block':
     processing_block_type = EmptyBlock
     dim_reduction_block_type = EmptyBlock
@@ -65,6 +68,7 @@ conv_experiment = ExperimentBuilder(network_model=custom_conv_net,
                                     num_epochs=args.num_epochs,
                                     weight_decay_coefficient=args.weight_decay_coefficient,
                                     use_gpu=args.use_gpu,
+                                    learning_rate = args.learning_rate,
                                     continue_from_epoch=args.continue_from_epoch,
                                     train_data=train_data_loader, val_data=val_data_loader,
                                     test_data=test_data_loader)  # build an experiment object
